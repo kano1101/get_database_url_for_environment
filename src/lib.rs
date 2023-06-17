9822b456183668cd044d168d41aa5508d03fe6e4
Akira Kano <a.kano1101@gmail.com>
2023-06-17 10:49:40 +0900

    region、secret_nameを引数として受け取るよう修正

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-pub async fn get_database_url_for_environment() -> anyhow::Result<String> {
+pub async fn get_database_url_for_environment<'a>(
+    region: &'static str,
+    secret_name: &'a str,
+) -> anyhow::Result<String> {
     if let Ok(mode) = std::env::var("LAMBDA_FUNCTION_MODE") {
         match mode.as_str() {
             "watch" => {
@@ -8,8 +11,6 @@ pub async fn get_database_url_for_environment() -> anyhow::Result<String> {
                 return Ok(url);
             }
             _ => {
-                let region = "ap-northeast-1";
-                let secret_name = "SecretsManager";
                 let input = Some((region, secret_name));
                 use establish_aws_mysql_sqlx::construct_url_for_aws;
                 let url = construct_url_for_aws(input).await.or_else(|_| {
