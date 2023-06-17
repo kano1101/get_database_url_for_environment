7745ddd3b11c50a1054f84b4b86daf62df59a51c
Akira Kano <a.kano1101@gmail.com>
2023-06-17 11:25:54 +0900

    regionをOptionに。

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub async fn get_database_url_for_environment<'a>(
-    region: &'static str,
+    region: Option<&'static str>,
     secret_name: &'a str,
 ) -> anyhow::Result<String> {
     if let Ok(mode) = std::env::var("LAMBDA_FUNCTION_MODE") {
@@ -11,6 +11,7 @@ pub async fn get_database_url_for_environment<'a>(
                 return Ok(url);
             }
             _ => {
+                let region = region.unwrap_or("ap-northeast-1");
                 let input = Some((region, secret_name));
                 use establish_aws_mysql_sqlx::construct_url_for_aws;
                 let url = construct_url_for_aws(input).await.or_else(|_| {
